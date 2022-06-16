f5948f8803a2d11967a8a95aaaebe8f6a5283e30
Sergio Cerdá Hervás <sergio@imaginagroup.com>
2022-06-16 16:55:31 +0200

    feat: Instalado sentry y activado sourcemap solo releases

--- apps/nx-vercel-test/src/App.tsx
+++ apps/nx-vercel-test/src/App.tsx
@@ -9,12 +9,34 @@ function App() {
     console.log({ ENV: process.env });
   }, []);
 
+  const onClick = () => {
+    console.log('clicked');
+
+    throw new Error(
+      `⚠️ Error controlado para 'Pruebas Setry' - ${new Date().toLocaleDateString()}`
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>{process.env.NX_TITLE}</h1>
 
         <p>{process.env.NX_DESCRIPTION}</p>
+
+        <button
+          onClick={onClick}
+          style={{
+            width: 'fit-content',
+            height: 'auto',
+            padding: '10px 15px',
+            borderRadius: '12px',
+            color: 'white',
+            backgroundColor: '#2A9B7D',
+          }}
+        >
+          ¡Generar error para Sentry!
+        </button>
 
         <img src={logo} className="App-logo" alt="logo" />
       </header>
--- apps/nx-vercel-test/src/index.tsx
+++ apps/nx-vercel-test/src/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import App from './App';
 
 import './index.css';
+
+Sentry.init({
+  dsn: 'https://97a21dc56bca487cac7f295197f82c84@o1267595.ingest.sentry.io/6509740',
+  integrations: [new BrowserTracing()],
+
+  tracesSampleRate: 0.5,
+});
 
 ReactDOM.render(
   <React.StrictMode>
