cfbb4a9da38a5670765631c1dfb4832646e9c3f9
Sergio Cerdá Hervás <sergio@imaginagroup.com>
2022-06-16 17:13:37 +0200

    feat: Activando release sentry

--- apps/nx-vercel-test/src/index.tsx
+++ apps/nx-vercel-test/src/index.tsx
@@ -8,8 +8,10 @@ import App from './App';
 import './index.css';
 
 Sentry.init({
-  dsn: 'https://97a21dc56bca487cac7f295197f82c84@o1267595.ingest.sentry.io/6509740',
+  release: 'nx-test-sentry@1.0.0',
+  environment: process.env.NODE_ENV,
   integrations: [new BrowserTracing()],
+  dsn: 'https://97a21dc56bca487cac7f295197f82c84@o1267595.ingest.sentry.io/6509740',
 
   tracesSampleRate: 0.5,
 });
